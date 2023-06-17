ee1ada790e9a898420de2619447d051f65ce3b9c
Stephen <night-build@kali.stephen>
2023-06-17 01:34:42 +0100

    worked on user registration

---
+++ mysociogram/contracts/UserRegistration.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract UserRegistration {
+      /** 
+     * @dev Private variables
+     */
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    struct User {
+        address walletAddress;
+        string name;
+    }
+
+    mapping(address => User) public users;
+
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+
+        _status = _NOT_ENTERED;
+    }
+
+    event UserRegistered(address indexed walletAddress, string name);
+    event UserUpdated(address indexed walletAddress, string name);
+    event UserDeleted(address indexed walletAddress);
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    function registerUser(string memory _name) public nonReentrant {
+        require(users[msg.sender].walletAddress == address(0), "User already registered");
+        
+        User storage newUser = users[msg.sender];
+        newUser.walletAddress = msg.sender;
+        newUser.name = _name;
+
+        emit UserRegistered(msg.sender, _name);
+    }
+
+    function getUserDetails() public view returns (string memory) {
+        User memory user = users[msg.sender];
+        require(user.walletAddress != address(0), "User not found");
+        
+        return (user.name);
+    }
+
+    function updateUser(string memory _name) public {
+        User storage user = users[msg.sender];
+        require(user.walletAddress != address(0), "User not found");
+
+        user.name = _name;
+
+        emit UserUpdated(msg.sender, _name);
+    }
+
+    function deleteUser() public {
+        require(users[msg.sender].walletAddress != address(0), "User not found");
+
+        delete users[msg.sender];
+
+        emit UserDeleted(msg.sender);
+    }
+}
