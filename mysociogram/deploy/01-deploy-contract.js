7397edb7ff18d1ff9ad9fdaabd9b490df62cef56
Stephen <night-build@kali.stephen>
2023-07-03 15:55:53 +0100

    governance contracts mainnet deployment

--- mysociogram/deploy/01-deploy-contract.js
+++ mysociogram/deploy/01-deploy-contract.js
@@ -7,8 +7,8 @@ const { verify } = require("../helper-functions")
 async function main() {
   // We get the contract to deploy
   console.log("Deploying contract...");
-  const Contract = await hre.ethers.getContractFactory('SocialDesgToken');
-  const contract = await Contract.deploy();
+  const Contract = await hre.ethers.getContractFactory('FoundersGovernance');
+  const contract = await Contract.deploy("0x99eb18F2c16d12eA40d30e064874bA33bD4296a1");
   
   await contract.deployed();
   console.log('Contract deployed to:', contract.address);
@@ -18,7 +18,7 @@ async function main() {
   if (
     !developmentChains.includes(network.name)
   ) {
-    await verify(contract.address, [])
+    await verify(contract.address, ["0x99eb18F2c16d12eA40d30e064874bA33bD4296a1"])
   }
 }
 
